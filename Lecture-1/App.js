03367256506feaa89850a733f1dfb90898aa5858
Pranjal Gupta <pranjal.gupta@rapidops.co>
2025-03-19 12:19:17 +0530

    lecture 1 completed

--- Lecture-1/App.js
+++ Lecture-1/App.js
@@ -1,25 +1,24 @@
 const heading1 = React.createElement(
-    "h1",
-    {
-      id: "heading1",
-      className: "heading1",
-    },
-    "Hello World1"
-  );
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "heading2",
-      className: "heading2",
-    },
-    "Hello World2"
-  );
-  const container = React.createElement("div", null, [heading1, heading2]);
-  const root = ReactDOM.createRoot(document.getElementById("root"));
+  "h1",
+  {
+    id: "heading1",
+    className: "heading1",
+  },
+  "Hello World1"
+);
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "heading2",
+    className: "heading2",
+  },
+  "Hello World2"
+);
+const container = React.createElement("div", { id: "container" }, [heading1, heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  // passing a react element to the root using render method
-  root.render(container);
+// passing a react element to the root using render method
+root.render(container);
 
 
 
-  
